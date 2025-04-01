37b90774d4788cc0070b0c3405504670c879a7fb
tiesen243 <ttien56906@gmail.com>
2025-04-01 08:11:09 +0700

    fix: thread

--- src/thread_2.c
+++ src/thread_2.c
@@ -6,7 +6,7 @@
 #include <unistd.h>
 
 int leds_fd, buttons_fd;
-static int time_ms = 1000;
+static int db = 1000;
 
 void sleep_ms(int ms) { usleep(ms * 1000); }
 
@@ -16,33 +16,27 @@ int main(int argc, char *argv[]) {
   const char *thread_params = "Button thread";
   pthread_t thread_1;
 
-  leds_fd = open("/dev/leds", O_RDONLY);
+  leds_fd = open("/dev/pwm", O_RDONLY);
   if (leds_fd < 0) {
-    perror("open leds");
-    exit(EXIT_FAILURE);
+    perror("open buzzer");
+    return EXIT_FAILURE;
   }
 
   buttons_fd = open("/dev/buttons", O_RDONLY);
   if (buttons_fd < 0) {
     perror("open buttons");
-    exit(EXIT_FAILURE);
+    return EXIT_FAILURE;
   }
 
   thread_1 =
       pthread_create(&thread_1, NULL, btn_polling, (void *)thread_params);
 
-  for (int i = 0; i < 4; i++)
-    ioctl(leds_fd, 0, i);
-
-  int led_id = 0;
   while (1) {
-    ioctl(leds_fd, 1, led_id);
-    sleep_ms(time_ms);
-    ioctl(leds_fd, 0, led_id);
-
-    led_id++;
-    if (led_id == 4)
-      led_id = 0;
+    int ret = ioctl(leds_fd, 1, db);
+    if (ret < 0) {
+      perror("set the frequency of the buzzer");
+      return EXIT_FAILURE;
+    }
   }
 
   close(buttons_fd);
@@ -52,29 +46,29 @@ int main(int argc, char *argv[]) {
 }
 
 void *btn_polling(void *param) {
-  int curr_btn[2], prev_btn[2] = {0, 0};
+  int i, curr_btn[2], prev_btn[2] = {0, 0};
 
-  while (1) {
+  for (;;) {
     int num = read(buttons_fd, curr_btn, sizeof(curr_btn));
     if (num != sizeof(curr_btn)) {
       perror("read buttons");
-      exit(EXIT_FAILURE);
+      pthread_exit(NULL);
     }
 
     if (curr_btn[0] != prev_btn[0]) {
       sleep_ms(300);
       prev_btn[0] = curr_btn[0];
-      time_ms += 50;
-      printf("K1 is pressed/released, time_ms = %dms\n", time_ms);
+      if (db < 3000)
+        db += 50;
+      printf("K1 is pressed, dB = %d\n", db);
     }
 
     if (curr_btn[1] != prev_btn[1]) {
       sleep_ms(300);
       prev_btn[1] = curr_btn[1];
-      time_ms -= 50;
-      if (time_ms < 100)
-        time_ms = 100;
-      printf("K2 is pressed/released, time_ms = %dms\n", time_ms);
+      if (db > 0)
+        db -= 50;
+      printf("K2 is pressed, dB = %d\n", db);
     }
   }
 
